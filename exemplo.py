d545acac99962f057bdb8242f518b79944d9da44
Ana Bossler <63399025+anabossler@users.noreply.github.com>
2021-04-14 16:36:38 +0200

    Add files via upload

---
+++ exemplo.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+nomes = ['rafael','joao','maria','ana','lucia']
+salarios = [1000,1100,1110,1200,1300]
+
+plt.figure(0)
+plt.plot(nomes,salarios, color='red')
+
+plt.figure(1)
+plt.bar(nomes,salarios)
+
+plt.savefig('grafico.png')
