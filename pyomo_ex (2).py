21be4d2490a75039529a7f42f38566a07b0f2624
Ana Bossler <63399025+anabossler@users.noreply.github.com>
2021-04-14 16:52:33 +0200

    Add files via upload

--- pyomo_ex (2).py
+++ pyomo_ex (2).py
@@ -4,18 +4,18 @@ from pyomo.opt import SolverFactory
 
 model = pyo.ConcreteModel()
 
-model.x = pyo.Var(within=Integers, bounds=(0,10))
+model.x = pyo.Var(bounds=(0,10))
 model.y = pyo.Var(bounds=(0,10))
 x = model.x
 y = model.y
 
-model.C1 = pyo.Constraint(expr= -x+2*y<=7)
+model.C1 = pyo.Constraint(expr= -x+2*y*x<=8)
 model.C2 = pyo.Constraint(expr= 2*x+y<=14)
 model.C3 = pyo.Constraint(expr= 2*x-y<=10)
 
-model.obj = pyo.Objective(expr= x+y, sense=maximize)
+model.obj = pyo.Objective(expr= x+y*x, sense=maximize)
 
-opt = SolverFactory('gurobi')
+opt = SolverFactory('ipopt', executable='C:\\ipopt\\bin\\ipopt.exe')
 opt.solve(model)
 
 model.pprint()
